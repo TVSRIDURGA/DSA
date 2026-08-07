c4e8901105723bbc16ebe551d9e54e85f61afbad
Talanki Venkata Sri Durga Ambica Bale <160519283+TVSRIDURGA@users.noreply.github.com>
2026-08-07 09:29:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (92.76%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        vector<int> hash(26, 0);
+
+        
+        for (char ch : s) {
+            hash[ch - 'a']++;
+        }
+
+        
+        int maxFreq = 0;
+        int letter = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (hash[i] > maxFreq) {
+                maxFreq = hash[i];
+                letter = i;
+            }
+        }
+
+        
+        if (maxFreq > (s.length() + 1) / 2)
+            return "";
+
+        
+        string res(s.length(), ' ');
+
+        
+        int idx = 0;
+        while (hash[letter] > 0) {
+            res[idx] = letter + 'a';
+            idx += 2;
+            hash[letter]--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            while (hash[i] > 0) {
+                if (idx >= s.length())
+                    idx = 1;
+
+                res[idx] = i + 'a';
+                idx += 2;
+                hash[i]--;
+            }
+        }
+
+        return res;
+    }
+};
