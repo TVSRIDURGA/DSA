d5ee4c09671ac55336a481f3f523edb3958baa73
Talanki Venkata Sri Durga Ambica Bale <160519283+TVSRIDURGA@users.noreply.github.com>
2026-08-07 11:02:37 +0530

    Time: 91 ms (23.12%) | Memory: 53.9 MB (5.26%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,3 +1,4 @@
+/* Brute Force aproach
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
@@ -13,5 +14,29 @@ public:
         }
         return count;
         
+    }
+};
+*/
+
+// optimal 
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        map<int,int>mpp;
+        mpp[0] =1;
+        int presum =0;
+        int count =0;
+        int n = nums.size();
+        for(int i =0 ;i<n ;i++){
+            presum = presum+nums[i];
+            int remove = presum -k;
+            count = count + mpp[remove];
+            mpp[presum] = mpp[presum]+1;
+        }
+        return count;
+
+
+
+
     }
 };
