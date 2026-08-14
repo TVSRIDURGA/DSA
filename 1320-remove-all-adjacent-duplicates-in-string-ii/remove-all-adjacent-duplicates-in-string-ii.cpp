9ba8eecac3cd14601e4bc1c010c908c9e692ca67
Talanki Venkata Sri Durga Ambica Bale <160519283+TVSRIDURGA@users.noreply.github.com>
2026-08-14 17:50:49 +0530

    Time: 8 ms (63.56%) | Memory: 14.3 MB (34.53%) - LeetSync

---
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        
+        stack<pair<char, int>> st;
+
+        for (auto x : s) {
+
+            if (!st.empty() && st.top().first == x) {
+                
+                st.top().second++;
+
+                if (st.top().second == k) {
+                    st.pop();
+                }
+            }
+            else {
+                st.push(make_pair(x, 1));
+            }
+        }
+
+        string ans = "";
+
+        while (!st.empty()) {
+            ans.append(st.top().second, st.top().first);
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
