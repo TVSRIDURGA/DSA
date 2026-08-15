19a5c2c043e6131ba12d0e8a9627b60e20f810dc
Talanki Venkata Sri Durga Ambica Bale <160519283+TVSRIDURGA@users.noreply.github.com>
2026-08-15 13:08:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (97.65%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,50 +1,42 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        vector<int> hash(26, 0);
-
-        
-        for (char ch : s) {
-            hash[ch - 'a']++;
+        vector<int>hash(26,0);
+        for(char ch:s){
+            hash[ch-'a']++;
         }
-
+        int maxfreq=0;
+        int letter =0;
         
-        int maxFreq = 0;
-        int letter = 0;
-
-        for (int i = 0; i < 26; i++) {
-            if (hash[i] > maxFreq) {
-                maxFreq = hash[i];
-                letter = i;
-            }
+        for(int i=0;i<26;i++){
+            if(hash[i]>maxfreq){
+            maxfreq = hash[i];
+            letter = i;
         }
-
-        
-        if (maxFreq > (s.length() + 1) / 2)
-            return "";
-
-        
-        string res(s.length(), ' ');
-
-        
-        int idx = 0;
-        while (hash[letter] > 0) {
-            res[idx] = letter + 'a';
-            idx += 2;
+        }
+        if(maxfreq > (s.length()+1)/2)
+        return "";
+         
+        string res(s.length(),' ');
+        int  idx =0;
+        while(hash[letter]> 0){
+            res[idx] = letter +'a';
+            idx= idx +2;
             hash[letter]--;
         }
 
-        for (int i = 0; i < 26; i++) {
-            while (hash[i] > 0) {
-                if (idx >= s.length())
-                    idx = 1;
+        for(int  i=0;i<26;i++){
+            while(hash[i]>0){
+                if(idx>=s.length()){
+                     idx= 1;
+                }
+               
 
-                res[idx] = i + 'a';
-                idx += 2;
+                res[idx] = i +'a';
+                idx= idx +2;
                 hash[i]--;
             }
         }
-
         return res;
     }
 };
