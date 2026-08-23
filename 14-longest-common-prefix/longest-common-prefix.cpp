f4c8ad2896bd4b29033d488bcf546ea1d6e52e12
Talanki Venkata Sri Durga Ambica Bale <160519283+TVSRIDURGA@users.noreply.github.com>
2026-08-23 20:23:28 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (38.90%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(), strs.end());
+
+        int n = strs.size();
+
+        string first = strs[0];
+        string last = strs[n - 1];
+        string ans = "";
+
+        for(int i = 0; i < first.size(); i++) {
+            if(first[i] != last[i])
+                break;
+            else
+                ans += first[i];
+        }
+
+        return ans;
+    }
+};
