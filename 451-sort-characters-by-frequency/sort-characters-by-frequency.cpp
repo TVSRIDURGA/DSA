8d53cefc9e4aec2de9c3b370205c8efcb3d345df
Talanki Venkata Sri Durga Ambica Bale <160519283+TVSRIDURGA@users.noreply.github.com>
2026-08-31 09:13:08 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (12.75%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -3,23 +3,24 @@ public:
     typedef pair<char,int>p;
     string frequencySort(string s) {
         vector<p> vec(123);
+
         for(char ch:s){
             int freq = vec[ch].second;
             vec[ch]={ch,freq+1};
         }
         auto lambda =[&](p &p1,p &p2){
-            return (p1.second > p2.second);
+            return (p1.second > p2.second );
         };
-        sort(begin(vec),end(vec),lambda);
-
-        string result="";
+        sort (begin(vec),end(vec),lambda);
+        string result ="";
         for(int i=0;i<=122;i++){
-            if(vec[i].second>0){
-                char ch= vec[i].first;
-                int freq  = vec[i].second;
-                string temp=string(freq,ch);
-                result = result+temp;
+            if(vec[i].second> 0){
+                 char ch  = vec[i].first;
+            int freq = vec[i].second;
+            string temp=string(freq,ch);
+            result = result + temp;
             }
+           
         }
         return result;
     }
