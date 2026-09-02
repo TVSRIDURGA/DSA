20d758267e23e9e15f6806a72caa323325a2c120
Talanki Venkata Sri Durga Ambica Bale <160519283+TVSRIDURGA@users.noreply.github.com>
2026-09-02 10:19:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (47.18%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     bool isVowel(char ch){
-      return   (ch =='a'|| ch =='e'|| ch =='i'|| ch =='o'|| ch =='u'||
-         ch =='A'|| ch =='E'|| ch =='I'|| ch =='O'|| ch =='U');
-    }
+        return  (ch=='a'||ch=='e'|| ch=='i'||ch=='o'||ch=='u'||
+                 ch=='A'||ch=='E'||ch=='I'|| ch=='O'||ch=='U'); }
     bool halvesAreAlike(string s) {
-        int n=s.size();
-        int m=n/2;
+        int n = s.size();
+        int m =n/2;
         int countL=0;
         int countR=0;
-        int i=0;
         int j=m;
+        int i=0;
+
+
+
         while(i<n/2 && j<n){
             if(isVowel(s[i])) countL++;
             if(isVowel(s[j])) countR++;
             i++;
             j++;
         }
-        return (countR ==countL);
+        return countL == countR;
+
+       
         
     }
 };
